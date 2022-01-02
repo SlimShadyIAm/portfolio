e35cdb2a5c706bec816f5c5f4b08ddd84891e8d0
Aamir Farooq <aamir@farooq.xyz>
2022-01-02 18:20:20 +0500

    Some fixes

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -13,8 +13,8 @@ const AboutMe = () => {
   return (
     <div className="bg-slim-blue py-12">
       <div className="container">
-        <div className="flex sm:flex-col md:flex-row text-center bg-white shadow-lg">
-          <div className="flex-1 border-gray-100 border-r px-16 py-8">
+        <div className="grid grid-cols-12 text-center bg-white shadow-lg">
+          <div className="col-span-12 md:col-span-6 border-gray-100 border-r px-16 py-8">
             <AboutMeEntry title="Who I Am" icon={faUser} skills={[
               "I'm an enthusiastic and passionate worker. I love building meaningful relationships when working with people.              "
             ]} />
@@ -34,7 +34,7 @@ const AboutMe = () => {
               "Cooking",
             ]} />
           </div>
-          <div className="flex-1 px-16 py-8">
+          <div className="col-span-12 md:col-span-6 px-16 py-8">
             <AboutMeEntry title="What I Do" icon={faLaptopCode} skills={[
               "My primary focus is on frontend design and development. I enjoy making beautiful, responsive and fast websites.            "
             ]} />
@@ -60,7 +60,7 @@ const AboutMe = () => {
 
 const AboutMeEntry: React.FC<AboutMeEntryProps> = ({ title, icon, skills }) => {
   return (
-    <div className="flex-1">
+    <div className="">
       {icon ? <p className="text-5xl text-blue-600 font-bold my-10"><FontAwesomeIcon icon={icon} /></p> : ""}
       <p className="text-md uppercase column-title mt-12 mb-6">{title}</p>
       {skills.map((skill, index) => <p key={index} className="text-md text-gray-800 my-1">{skill}</p>)}
--- src/components/HeroHome.tsx
+++ src/components/HeroHome.tsx
@@ -7,13 +7,13 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
 const HeroHome = () => {
   return (
     <div className="flex justify-center">
-      <div className="py-4 mx-24 flex">
+      <div className="py-4 lg:mx-24 md:flex">
         <div className="flex grow mx-12 my-10 items-center justify-center">
           <img src={pfp} alt="Aamir" className="w-64 h-64 rounded-full shadow-md hover:shadow-lg hover:-translate-y-1 transition-all" />
         </div>
         <div className="grow mx-12 my-5">
-          <p className="text-5xl text-right intro-text uppercase"><span className="mask">Aamir<br />Farooq</span></p>
-          <p className="text-2xl text-white text-right font-bold my-2">Designer. Developer. Dreamer.</p>
+          <p className="text-5xl md:text-right intro-text uppercase"><span className="mask">Aamir<br />Farooq</span></p>
+          <p className="text-2xl text-white md:text-right font-bold my-2">Designer. Developer. Dreamer.</p>
           <div className="flex flex-row space-x-2 my-4 justify-center">
             <a href="https://github.com/SlimShadyIAm" className="px-2 py-3 text-white text-2xl text-center flex-1 bg-transparent-grey rounded-md shadow-md hover:shadow-lg transition-shadow flex justify-center items-center"><FontAwesomeIcon icon={faGithub} /></a>
             <a href="https://twitter.com/slimshadydev" className="px-2 py-3 text-white text-2xl text-center flex-1 bg-transparent-grey rounded-md shadow-md hover:shadow-lg transition-shadow flex justify-center items-center"><FontAwesomeIcon icon={faTwitter} /></a>
