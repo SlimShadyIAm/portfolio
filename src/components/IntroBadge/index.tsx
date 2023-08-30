eb8e3377b8eb72f426d7ef2ec83d0968fd0863ed
SlimShadyIAm <aamir@farooq.xyz>
2023-08-30 17:35:18 +0200

    Slight tweaks

--- src/components/IntroBadge/index.tsx
+++ src/components/IntroBadge/index.tsx
@@ -7,8 +7,8 @@ type Props = {
 
 const IntroBadge = ({ icon: IconComponent, text }: Props) => {
   return (
-    <div className="pretty-ring flex items-center gap-2 self-start rounded-full px-3 py-1.5 text-sm text-zinc-400">
-      <IconComponent className="fill-zinc-500 text-zinc-300" size={18} />
+    <div className="pretty-ring flex items-center gap-2 self-start rounded-full px-3 py-1.5 text-sm text-zinc-400/75">
+      <IconComponent className="fill-zinc-600/75 text-zinc-400/75" size={18} />
       <span className="font-medium">{text}</span>
     </div>
   );
