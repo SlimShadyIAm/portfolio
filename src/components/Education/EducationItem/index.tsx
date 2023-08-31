c314f10429cee403aae1f287fd0a30a55822b7a8
SlimShadyIAm <aamir@farooq.xyz>
2023-08-31 19:27:25 +0200

    Finish Education card

--- src/components/Education/EducationItem/index.tsx
+++ src/components/Education/EducationItem/index.tsx
@@ -5,30 +5,35 @@ type Props = {
   src: string;
   alt: string;
   last?: boolean;
-  // title: string;
-  // description: string;
-  // years: string;
+  title: string;
+  description: string;
+  years: string;
 };
 
 const EducationItem = ({
   src,
   alt,
-  last = false, // title,
-  // years,
-} // description,
-: Props) => {
-  // const lineCss =
-  //   'after:mx-auto after:my-2 after:block after:h-10 after:w-[1px] after:rounded-full after:bg-slate-300/20 after:shadow-sm after:shadow-slate-800/5 after:backdrop-blur';
+  last = false,
+  title,
+  years,
+  description,
+}: Props) => {
+  const lineCss =
+    'after:mx-auto after:my-2 after:block after:h-10 after:w-[1px] after:rounded-full after:bg-slate-300/20 after:shadow-sm after:shadow-slate-800/5 after:backdrop-blur';
 
-  const lineCss = '';
   return (
-    <div className={`flex flex-row ${last ? '' : lineCss}`}>
-      <div className="pretty-ring row self-start rounded-full p-1">
-        <Image src={src} width={24} height={24} alt={alt} />
+    <div className="flex w-full flex-row gap-4 p-1">
+      <div className={`${last ? '' : lineCss}`}>
+        <div className="pretty-ring row p-1 self-start rounded-full">
+          <Image src={src} width={24} height={24} alt={alt} />
+        </div>
       </div>
-      <div>
-        <h1>DTU</h1>
-        <p>2020-2020</p>
+      <div className="flex w-full flex-col gap-1">
+        <div className="flex w-full flex-row justify-between">
+          <h1 className="text-md font-semibold text-slate-300/90">{title}</h1>
+          <p className="text-xs font-bold text-slate-600">{years}</p>
+        </div>
+        <p className="text-sm text-slate-500">{description}</p>
       </div>
     </div>
   );
--- src/components/Education/index.tsx
+++ src/components/Education/index.tsx
@@ -7,13 +7,26 @@ type Props = {};
 
 const Education = (props: Props) => {
   return (
-    <div className="pretty-ring flex w-full flex-col items-start justify-center self-start rounded-md p-4 gap-4">
-      <div className="align-center ml-1 flex flex-row gap-4 self-start">
+    <div className="pretty-ring flex w-[50%] flex-col self-start rounded-md p-6">
+      <div className="align-center mb-6 flex flex-row gap-4 self-start">
         <Briefcase className="fill-slate-600/75 text-sm text-slate-400/75" />
-        <h2 className="text-slate-200/50">Education</h2>
+        <h2 className="font-medium text-slate-200/90">Education</h2>
       </div>
-      <EducationItem src="/images/dtu.svg" alt="DTU Logo" />
-      <EducationItem src="/images/twente.svg" alt="UT Logo" last />
+        <EducationItem
+          src="/images/dtu.svg"
+          alt="DTU Logo"
+          title="Technical University of Denmark"
+          years="2021 – Current"
+          description="MSc. Computer Science and Engineering (spec. Software Engineering)"
+        />
+        <EducationItem
+          last
+          src="/images/twente.svg"
+          alt="UT Logo"
+          title="University of Twente"
+          years="2018 – 2020"
+          description="BSc. Technical Computer Science"
+        />
     </div>
   );
 };
