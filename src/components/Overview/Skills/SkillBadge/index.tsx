09bae7c7098d5a148ce002566f73e2dacf37f294
SlimShadyIAm <aamir@farooq.xyz>
2023-09-02 13:03:18 +0200

    Improve performance

--- src/components/Overview/Skills/SkillBadge/index.tsx
+++ src/components/Overview/Skills/SkillBadge/index.tsx
@@ -8,7 +8,7 @@ type Props = {
 const SkillBadge = ({ name }: Props) => {
   return (
     <div className="pretty-ring bg-slate-700/5 flex items-center gap-2 self-start rounded-full px-1.5 py-1.5 pr-3 text-sm text-slate-200/80">
-     <Image className='pretty-ring rounded-full' src={`/images/logos/${name.toLowerCase()}.svg`} width={28} height={28} alt={`${name} logo`}/>
+     <Image className='rounded-full' src={`/images/logos/${name.toLowerCase()}.png`} width={28} height={28} alt={`${name} logo`}/>
       <span>{name}</span>
     </div>
   );
