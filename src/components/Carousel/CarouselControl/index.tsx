28514306e8802474b073b2bdebfeff47053c8b81
SlimShadyIAm <aamir@farooq.xyz>
2023-09-04 19:21:53 +0200

    Improve implementation

---
+++ src/components/Carousel/CarouselControl/index.tsx
@@ -0,0 +1,29 @@
+import { ArrowLeft, ArrowRight } from 'lucide-react';
+import React from 'react';
+
+type Props = {
+  isLeft?: boolean;
+  handleNext: () => void;
+  handlePrevious: () => void;
+};
+
+const CarouselControl = ({
+  isLeft = false,
+  handleNext,
+  handlePrevious,
+}: Props) => {
+  return (
+    <button
+      type="button"
+      onClick={!isLeft ? handleNext : handlePrevious}
+      className={`absolute z-10 flex h-10 w-10 items-center justify-center rounded-full bg-black text-2xl text-white opacity-75 ${
+        isLeft ? 'left-8' : 'right-8'
+      }`}
+      style={{ top: '40%' }}
+    >
+      {isLeft ? <ArrowLeft /> : <ArrowRight />}
+    </button>
+  );
+};
+
+export default CarouselControl;
--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,6 +1,7 @@
 import { RefObject, createRef, useState } from 'react';
 import styles from './styles.module.css';
 import Image from 'next/image';
+import CarouselControl from './CarouselControl';
 
 type Props = {
   images: string[];
@@ -15,11 +16,8 @@ const Carousel = ({ images }: Props) => {
     },
     {}
   );
-  // Some validation for checking the array length could be added if needed
   const totalImages = images.length;
 
-  // Below functions will assure that after last image we'll scroll back to the start,
-  // or another way round - first to last in previousImage method.
   const nextImage = () => {
     if (currentImage >= totalImages - 1) {
       scrollToImage(0);
@@ -37,51 +35,37 @@ const Carousel = ({ images }: Props) => {
   };
 
   const scrollToImage = (i: number) => {
-    // First let's set the index of the image we want to see next
     setCurrentImage(i);
 
     refs[i].current?.scrollIntoView({
-      //     Defines the transition animation.
       behavior: 'smooth',
-      //      Defines vertical alignment.
       block: 'nearest',
-      //      Defines horizontal alignment.
       inline: 'start',
     });
   };
 
-  const arrowStyle =
-    'absolute text-white text-2xl z-10 bg-black h-10 w-10 rounded-full opacity-75 flex items-center justify-center';
-
-  const sliderControl = (isLeft: boolean) => (
-    <button
-      type="button"
-      onClick={isLeft ? previousImage : nextImage}
-      className={`${arrowStyle} ${isLeft ? 'left-2' : 'right-2'}`}
-      style={{ top: '40%' }}
-    >
-      <span role="img" aria-label={`Arrow ${isLeft ? 'left' : 'right'}`}>
-        {isLeft ? '◀' : '▶'}
-      </span>
-    </button>
-  );
-
   return (
-        <div className={styles.carousel}>
-          {sliderControl(true)}
-          {images.map((img, i) => (
-            <div className="w-full flex-shrink-0" key={img} ref={refs[i]}>
-              <Image
-                src={`/images/projects/${img}`}
-                className="w-full object-contain"
-                alt="Demo demo image"
-                width={1000}
-                height={530}
-              />
-            </div>
-          ))}
-          {sliderControl(false)}
+    <div className={styles.carousel + ' rounded-lg'}>
+      <CarouselControl
+        isLeft
+        handleNext={nextImage}
+        handlePrevious={previousImage}
+      />
+      {images.map((img, i) => (
+        <div className="w-full flex-shrink-0" key={img} ref={refs[i]}>
+          <Image
+            src={`/images/projects/${img}`}
+            alt="Demo demo image"
+            width={1000}
+            height={530}
+          />
         </div>
+      ))}
+      <CarouselControl
+        handleNext={nextImage}
+        handlePrevious={previousImage}
+      />
+    </div>
   );
 };
 
