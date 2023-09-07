93cfe7cb7b1b2261c9847f6d3295e3ad1a08bcb7
SlimShadyIAm <aamir@farooq.xyz>
2023-09-07 19:51:23 +0200

    Add theme color meta tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,19 @@ import Overview from '@/components/sections/Overview';
 import Projects from '@/components/sections/Projects';
 import Divider from '@/components/Divider';
 import Footer from '@/components/Footer';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: '#020617' },
+    { media: '(prefers-color-scheme: dark)', color: '#020617' },
+  ],
+};
 
 const Home = () => {
   return (
     <div
-      className={`mx-auto flex min-h-full w-full flex-col gap-24 bg-slate-950 pt-24 pb-12 ${styles.grid}`}
+      className={`mx-auto flex min-h-full w-full flex-col gap-24 bg-slate-950 pb-12 pt-24 ${styles.grid}`}
     >
       <div className="container flex flex-col items-start">
         <Intro />
