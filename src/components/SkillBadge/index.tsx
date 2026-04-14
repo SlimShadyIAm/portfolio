2011a285b985075ed776394926682d568847c248
Aamir Farooq <aamir@farooq.xyz>
2026-04-14 21:43:56 +0200

    Add more skills

--- src/components/SkillBadge/index.tsx
+++ src/components/SkillBadge/index.tsx
@@ -1,21 +1,22 @@
 import Image from 'next/image';
 import React from 'react';
+import { Skill } from '@/components/sections/Overview/Skills';
 
 type Props = {
-  name: string;
+  skill: Skill;
 };
 
-const SkillBadge = ({ name }: Props) => {
+const SkillBadge = ({ skill }: Props) => {
   return (
     <div className="pretty-ring flex items-center gap-2 self-start rounded-full bg-slate-700/5 px-1.5 py-1.5 pr-3 text-sm text-slate-200/80">
       <Image
         className="rounded-full"
-        src={`/images/logos/${name.toLowerCase()}.png`}
+        src={`/images/logos/${skill.icon}`}
         width={28}
         height={28}
-        alt={`${name} logo`}
+        alt={`${skill.icon} logo`}
       />
-      <span>{name}</span>
+      <span>{skill.name}</span>
     </div>
   );
 };
--- src/components/sections/Overview/Skills/index.tsx
+++ src/components/sections/Overview/Skills/index.tsx
@@ -3,31 +3,35 @@ import { Sparkles } from 'lucide-react';
 import SkillBadge from '@/components/SkillBadge';
 import OverviewCard from '@/components/OverviewCard';
 
+export type Skill = { name: string; icon: string };
+
 const Skills = () => {
-  const skills = [
-    'React',
-    'TypeScript',
-    'JavaScript',
-    'Jest',
-    'Cypress',
-    'TailwindCSS',
-    'HTML',
-    'CSS',
-    'Python',
-    'Java',
-    'Figma',
-    'Git',
-    'Docker',
-    'SQL',
-    'Vim',
-    'Linux',
+  const skills: Skill[] = [
+    { name: 'React', icon: 'react.png' },
+    { name: 'Next.js', icon: 'nextjs.png' },
+    { name: 'Angular', icon: 'angular.png' },
+    { name: 'TypeScript', icon: 'typescript.png' },
+    { name: 'JavaScript', icon: 'javascript.png' },
+    { name: 'C#', icon: 'csharp.png' },
+    { name: 'Cypress', icon: 'cypress.png' },
+    { name: 'TailwindCSS', icon: 'tailwindcss.png' },
+    { name: 'HTML', icon: 'html.png' },
+    { name: 'CSS', icon: 'css.png' },
+    { name: 'Python', icon: 'python.png' },
+    { name: 'Java', icon: 'java.png' },
+    { name: 'Figma', icon: 'figma.png' },
+    { name: 'Git', icon: 'git.png' },
+    { name: 'Docker', icon: 'docker.png' },
+    { name: 'SQL', icon: 'sql.png' },
+    { name: 'Vim', icon: 'vim.png' },
+    { name: 'Linux', icon: 'linux.png' },
   ];
 
   return (
     <OverviewCard icon={Sparkles} title="Skills">
       <div className="flex flex-wrap gap-3 p-1">
         {skills.map((skill) => (
-          <SkillBadge key={skill} name={skill} />
+          <SkillBadge key={skill.name} skill={skill} />
         ))}
       </div>
     </OverviewCard>
